77fd2d248b1e4e8bc638223eafaf317ca95e4194
unknown <marselina88@list.ru>
2022-05-19 18:14:44 +0300

    cart_order
    начало карточек заказов для админа

--- app/index.php
+++ app/index.php
@@ -28,36 +28,25 @@
 <div class="container-fluid col-xl-12">
 <div class="d-flex flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">
     <div class="edit__title title_padding darkBlue fontSans size32px"><a href="" class="">Управление товарами</a></div>
-     <div class="edit__title title_padding darkBlue fontSans size32px"><a href="" class="orange">Управление категориями</a></div>
-      <div class="edit__title title_padding darkBlue fontSans size32px"><a href="">Управление заказами</a></div>
+     <div class="edit__title title_padding darkBlue fontSans size32px"><a href="" class="">Управление категориями</a></div>
+      <div class="edit__title title_padding darkBlue fontSans size32px"><a href="" class="orange">Управление заказами</a></div>
 
-</div><div class="edit__title title_padding fontSans size29px"><a href="" class="orange">Добавить категорию</a></div>
-
-     <form class="d-flex col-xl-12 flex-column">
-        <div class="d-flex flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">
-     <div class="edit__title title_padding col-xl-4 fontSans "><div class="text-center"><a href="" class="size32px darkBlue">Белая выпечка</a></div>
- <div class="edit-cart__operation view-product-right-info_padding"><a href="" class="burgundy size20px">Удалить</a></div>
-         <div class="d-flex align-items-center">
-        <label for="category3" class="size20px">Изменить</label>
-             <div class="edit-cart__item-input"><input type="text" name="category3" value="Белая выпечка" class="brownGrey edit-cart__item-input"></div></div></div>
-      <div class="edit__title title_padding col-xl-4  fontSans"><div class="text-center"><a href="" class="size32px darkBlue">Пирожные</a></div>
-   <div class="edit-cart__operation view-product-right-info_padding"><a href="" class="burgundy size20px">Удалить</a></div>
-         <div class="d-flex align-items-center">
-        <label for="category3" class="size20px">Изменить</label>
-             <div class="edit-cart__item-input"><input type="text" name="category3" value="Пирожные" class="brownGrey edit-cart__item-input"></div></div></div>
-
-       <div class="edit__title title_padding col-xl-4  fontSans">
-        <div class="text-center"><a href="" class="darkBlue size32px">Черный хлеб</a></div>
-         <div class="edit-cart__operation view-product-right-info_padding"><a href="" class="burgundy size20px">Удалить</a></div>
-         <div class="d-flex align-items-center">
-        <label for="category3" class="size20px">Изменить</label>
-             <div class="edit-cart__item-input"><input type="text" name="category3" value="Черный хлеб" class="brownGrey edit-cart__item-input"></div></div></div>
-             </div>
-             <div class="d-flex edit__title title_padding justify-content-end">
-             <button type="button" class=" btn btn-success col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6 size20px">Сохранить изменения</button></div>
-         </form>
-       </div>
 </div>
+<div class="wrap-cart-order">
+    <div class="cart-order col-xl-4">
+        <div class="cart-order_padding">
+        <div class="">Заказ №<div class="">132</div></div>
+            <div class="">Дата заказа</div>
+            <div class="">Заказчик: <div class="">35(Пёс)</div></div>
+            <div class="">Телефон: <div class="">8-940-88-88</div></div>
+            <div class="">Статус<div class="">доставлено</div></div>
+            <div class=""><a href="" class="table-order__del burgundyLight">Удалить</a></div>
+           
+</div>
+    </div>
+    </div>
+</div>
+
    
 <?php require_once('admin_footer.php');
 //require_once('cart-full.php');
