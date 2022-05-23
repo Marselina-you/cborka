21ee5a9867eaf9faa2095d14dedf71c2c6f2efca
unknown <marselina88@list.ru>
2022-05-23 14:49:09 +0300

    cart-order
    доделано

--- app/index.php
+++ app/index.php
@@ -32,17 +32,39 @@
       <div class="edit__title title_padding darkBlue fontSans size32px"><a href="" class="orange">Управление заказами</a></div>
 
 </div>
-<div class="wrap-cart-order">
-    <div class="cart-order col-xl-4">
+<div class="cart-orders d-flex flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">
+<div class="wrap-cart-order col-xl-3 col-lg-4  bottom_padding10">
+    <div class="cart-order">
         <div class="cart-order_padding">
-        <div class="">Заказ №<div class="">132</div></div>
-            <div class="">Дата заказа</div>
-            <div class="">Заказчик: <div class="">35(Пёс)</div></div>
-            <div class="">Телефон: <div class="">8-940-88-88</div></div>
-            <div class="">Статус<div class="">доставлено</div></div>
-            <div class=""><a href="" class="table-order__del burgundyLight">Удалить</a></div>
+        <div class="cart-order__name d-flex align-items-center"><div class="cart-order__name_padding brownDark size20px">Заказ №</div><div class="brownDark size22px font-weight-bold cart-order__name_padding">132</div></div>
+        <div class="cart-order__name d-flex align-items-center"><div class="brownDark size20px cart-order__name_padding">Дата заказа</div><div class="brownDark size22px font-weight-bold cart-order__name_padding">17.09.21</div></div>
+      
+        <div class="cart-order__name d-flex align-items-center"><div class="cart-order__name_padding brownDark size20px">Заказчик:</div><div class="brownDark size22px font-weight-bold cart-order__name_padding">35(Пёс)</div></div>
+            <div class="cart-order__name d-flex align-items-center"><div class="cart-order__name_padding brownDark size20px">Телефон: </div><div class="brownDark size22px font-weight-bold cart-order__name_padding">8-940-88-88</div></div>
+            <div class="cart-order__name d-flex">
+                <div class="cart-order__name_padding brownDark size20px align-items-center">Статус:</div>
+                <div class="brownDark size22px font-weight-bold cart-order__name_padding">доставлено</div>
+            </div>
+            <div class="cart-order__name_padding d-flex justify-content-center"><button type="button" class="btn btn-danger"><a href="" class="table-order__del white">Удалить</a></button></div>
            
 </div>
+    </div>
+    </div>
+<div class="wrap-cart-order col-xl-3 bottom_padding10">
+    <div class="cart-order">
+        <div class="cart-order_padding">
+        <div class="cart-order__name d-flex"><div class="cart-order__name_padding brownDark size20px">Заказ №</div><div class="brownDark size22px font-weight-bold cart-order__name_padding">132</div></div>
+        <div class="cart-order__name d-flex"><div class="brownDark size20px cart-order__name_padding">Дата заказа</div><div class="brownDark size22px font-weight-bold cart-order__name_padding">17.09.21</div></div>
+      
+        <div class="cart-order__name d-flex"><div class="cart-order__name_padding brownDark size20px">Заказчик:</div><div class="brownDark size22px font-weight-bold cart-order__name_padding">35(Пёс)</div></div>
+            <div class="cart-order__name d-flex"><div class="cart-order__name_padding brownDark size20px">Телефон: </div><div class="brownDark size22px font-weight-bold cart-order__name_padding">8-940-88-88</div></div>
+            <div class="cart-order__name d-flex">
+                <div class="cart-order__name_padding brownDark size20px">Статус:</div>
+                <div class="brownDark size22px font-weight-bold cart-order__name_padding">доставлено</div>
+            </div>
+            <div class="cart-order__name_padding d-flex justify-content-center"><button type="button" class="btn btn-danger"><a href="" class="table-order__del white">Удалить</a></button></div>
+           
+</div></div>
     </div>
     </div>
 </div>
