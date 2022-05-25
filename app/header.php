d4be3756e5e52e5d86759076d299c416c1f7998d
unknown <marselina88@list.ru>
2022-05-25 18:21:21 +0300

    start
    стартовая страница без мобил стилей

--- app/header.php
+++ app/header.php
@@ -35,10 +35,5 @@
         </div></div>
             
  </div>
- <div class="header-logo-menu col-xl-10 col-lg-10 offset-xl-1 offset-lg-1 col-md-10 offset-md-1 col-sm-10 offset-sm-1 d-flex justify-content-between align-items-center">
-      <div class="header-logo-menu__logo"><img src="../images/logo.png" alt=""></div>
-
-      <div class="header-logo-menu__menu"><img src="../images/menu.png" alt=""></div>
-     
- </div>
+ 
 </div>
--- app/index.php
+++ app/index.php
@@ -8,7 +8,8 @@
     <title>title</title>
 </head>
 <body>
-<?php //require_once('admin_header.php');
+<?php require_once('header.php');
+//require_once('admin_header.php');
 //require_once('admin_index.php');
 //require_once('admin_menu.php');
 //require_once('admin_products_manage.php');
@@ -19,104 +20,168 @@
 //require_once('admin_order_cart.php');
 //require_once('admin_footer.php');
 
-require_once('header.php');
+
 //require_once('registr.php');
 //require_once('registr-end.php');
 //require_once('enter.php');
+//require_once('user_index.php');
 
 
 //require_once('cart-empty.php');
 //require_once('cart-full.php');
 //require_once('cart-order-end.php');
 //require_once('cart-order-form.php');
+//require_once('start.php');
+
+
 ?>
+
+<?php //require_once('start.php');    ?>   
 <div class="wrap-content">
-      <div class="user-index">
-         <div class="content-empty-text col-xl-6 offset-xl-1">
-            <div class="title size35px brownDark fontTahoma letter-space">Личный кабинет</div>
-            <div class="edit-cart__content_padding size35px brownDark fontTahoma letter-space">Привет, Собака!</div>
-            
-        
-         <div class="d-flex flex-xl-row flex-lg-row flex-md-column flex-sm-column flex-column">
-    <div class="edit__title  user-index_padding darkBlue fontSans size32px"><a href="" class="">Доставки</a></div>
-     <div class="edit__title user-index_padding darkBlue fontSans size32px"><a href="" class="">Покупки</a></div>
-      <div class="edit__title user-index_padding darkBlue fontSans size32px"><a href="" class="">Мои данные</a></div>
+    <div class="main">
+      <div class="main-start">
+<div class="main-header-logo header-logo-menu col-xl-10 col-lg-10 offset-xl-1 offset-lg-1 col-md-10 offset-md-1 col-sm-10 offset-sm-1 d-flex justify-content-between align-items-center">
+      <div class="header-logo-menu__logo"><img src="../images/logo.png" alt=""></div>
 
-</div> </div>
-         <div class="user-index-actual">
-            <div class="undertitle size29px fontTahoma text-center letter-space"><a href="">Доставки</a></div>
-          
-            
-              <div class="user-index-actual__data size29px fontArial text-center brownDark">Заказ от 29 апреля</div>
-               
-            <div class="user-index-actual__num size24px fontArial text-center darkBlue cart-order__name_padding">235098766</div>
-            <div class="wrap-user-index-actual-cart d-flex justify-content-center  col-xl-4">
-                <div class="user-index-actual-cart d-flex flex-column align-items-center ">
-                    <div class="user-index-actual-cart__img cart-order__name_padding d-flex justify-content-center"><img src="../images/page.png"></div>
-                    <div class="user-index-actual-cart__name cart-order__name_padding size29px fontSans brownDark">Пирожные. Ассорти</div>
-                </div>
-            </div>
-            <div class="user-index-actual-info d-flex  flex-column align-items-center  col-xl-4">
-                <div class="user-index-actual-info__condition col-xl-4 white cart-order__name_padding size24px text-center">В пути</div>
-                <div class="cart-order__name_padding fontSans size22px">Будет доставлено 29 апреля 17.00</div>
-            </div>
-         </div>
-          <div class="user-index-actual">
-            <div class="undertitle size29px fontTahoma text-center letter-space"><a href="">Покупки</a></div>
-          
-            
-              <div class="user-index-actual__data size29px fontArial text-center brownDark">Заказ от 29 апреля</div>
-               
-            <div class="user-index-actual__num size24px fontArial text-center darkBlue cart-order__name_padding">235098766</div>
-            <div class="wrap-user-index-actual-cart d-flex justify-content-center  col-xl-4">
-                <div class="user-index-actual-cart d-flex flex-column align-items-center ">
-                    <div class="user-index-actual-cart__img cart-order__name_padding d-flex justify-content-center"><img src="../images/page2.png"></div>
-                    <div class="user-index-actual-cart__name cart-order__name_padding size29px fontSans brownDark">Пирожные. Ассорти</div>
-                </div>
-            </div>
-            <div class="user-index-actual-info d-flex  flex-column align-items-center  col-xl-4">
-                <div class="user-index-shop-info__condition col-xl-4 white cart-order__name_padding size24px text-center">Доставлено</div>
-               
-            </div>
-         </div>
+      <div class="header-logo-menu__menu"><img src="../images/menu.png" alt=""></div>
+     
+ </div>
+ <div class="main-start col-xl-6 offset-xl-1 col-lg-6 offset-lg-1  col-md-6 offset-md-1 col-sm-6 offset-sm-1 col-6 offset-3">
+    <div class="main-start-name wrap-main-orderEnd-content-amount">
+    <div class="main-start-name__item1 bottom_padding10 fontTahoma size45px brownDark">ЗА БАТОНОМ </div>
+ <div class="main-start-name__item2 bottom_padding10 fontTahoma size45px brownDark">С ПЫЛУ С ЖАРУ</div>
+ <div class="main-start-name__item3 size45px fontTahoma brownDark">ИЗ РУССКОЙ ПЕЧИ</div></div>
+ <div class="main-start-name__item4 fontTahoma size26px brownDark">мини-пекарня</div>
+ </div></div>
+<div class="main-grandmenu">
+    <div class="main-grandmenu-block col-xl-4 d-flex flex-column align-items-center">
+        <div class="main-grandmenu-block__img d-flex flex-column text-center">
+             <a href="" class=""><img src="../images/bread.png"></a></div>
+        <div class="main-cartFull-list-item__info-weight_padding main-grandmenu-block__name "> <a href="" class="brownDark fontSans size29px font-weight-bold">ЧЕРНЫЙ ХЛЕБ</a></div>
+        <div class="main-grandmenu-block__desc line-height1 brownDark  text-center"><a href="" class="brownDark fontSegoePrint size29px">от бородинского до</br>
+трансильванского</a></div>
+<div class="wrap-main-grandmenu-block__btn main-cartFull-list-item__info-weight_padding">
+        <div class="main-grandmenu-block__btn icons__item_padding main-btn_hover">
+            <a href="" class="main_btn col-xl-6 size29px white">перейти</a>
+        </div></div>
+       </div>
+       <div class="main-grandmenu-block col-xl-4 d-flex flex-column align-items-center">
+        <div class="main-grandmenu-block__img d-flex flex-column text-center">
+             <a href=""><img src="../images/cake.png"></a></div>
+        <div class="main-cartFull-list-item__info-weight_padding main-grandmenu-block__name font-weight-bold"> <a href="" class="brownDark fontSans size29px">ПИРОЖНЫЕ</a></div>
+        <div class="main-grandmenu-block__desc line-height1 brownDark  text-center"><a href="" class="brownDark fontSegoePrint size29px">от корзиночки</br>
+   до картошки</a></div>
+<div class="wrap-main-grandmenu-block__btn main-cartFull-list-item__info-weight_padding">
+        <div class="main-grandmenu-block__btn icons__item_padding main-btn_hover">
+            <a href="" class="main_btn col-xl-6 size29px white">перейти</a>
+        </div></div>
+       </div>
 
-         <div class="user-index-actual">
-            <div class="undertitle size29px fontTahoma text-center letter-space"><a href="">Мои данные</a></div>
-          <form class="main-enter-content__form form_padding col-xl-6 offset-xl-1 d-flex flex-column"  method="post">
-      <div class="content-form__data d-flex align-items-center justify-content-between flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">
-          <label for="email" class="content-form__data-title size35px col-xl-4">E-mail:</label>
-          <div class="wrap-content-form__data-input col-xl-8">
-            <input type="text" name="email" class="content-form__data-input col-xl-12" value="masrghj@fgn"></div>
-      </div>
-      <div class="content-form__data d-flex align-items-center justify-content-between flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">
-          <label for="name" class="content-form__data-title size35px col-xl-4">Имя:</label>
-          <div class="wrap-content-form__data-input col-xl-8">
-            <input type="text" name="name" class="content-form__data-input col-xl-12" value="собака"></div>
-      </div>
+        <div class="main-grandmenu-block col-xl-4 d-flex flex-column align-items-center">
+        <div class="main-grandmenu-block__img text-center">
+             <a href=""><img src="../images/loaf.png"></a></div>
+        <div class="main-cartFull-list-item__info-weight_padding main-grandmenu-block__name "> <a href="" class="brownDark fontSans size29px font-weight-bold">БЕЛАЯ ВЫПЕЧКА</a></div>
+        <div class="main-grandmenu-block__desc line-height1 brownDark  text-center"><a href="" class="main_btn brownDark fontSegoePrint size29px">от багета до</br>
+     чиабатты</a></div>
+<div class="wrap-main-grandmenu-block__btn main-cartFull-list-item__info-weight_padding">
+        <div class="main-grandmenu-block__btn icons__item_padding main-btn_hover">
+            <a href="" class="col-xl-6 size29px white">перейти</a>
+        </div></div>
+       </div>
+
       
-      <div class="content-form__data d-flex align-items-center justify-content-between flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">
-          <label for="parol2" class="content-form__data-title size35px col-xl-4">Телефон:</label>
-          <div class="wrap-content-form__data-input col-xl-8">
-            <input type="text" name="parol2" class="content-form__data-input col-xl-12" value="8-0960-98-98"></div>
-      </div>
-<div class="content-form__data d-flex align-items-center justify-content-end">
-   <div class="col-xl-7 col-lg-4 col-md-4 col-sm-4 col-6  d-flex justify-content-end">
-<button class="content-order-end__click__btn btn btn-success size29px col-xl-6" type="button">Сохранить</button></div></div>
-   </form>
-   
-            
-              
-         </div>
-         
-         
+     </div>
+     <div class="main-action-slider d-flex justify-content-center">
+ <div class="main d-flex flex-column align-items-center justify-content-center text-center">
 
-         
+    <div class="action-slider__title d-flex flex-column">
+   <div class="content-empty-slider__title brownDark size45px fontTahoma letter-space d-flex justify-content-center border_bottom font-weight-bold">НЕ УПУСТИ АКЦИИ И ДЕГУСТАЦИИ!!!</div>
+   <div class="content-empty-slider__title white size35px fontSegoePrint letter-space d-flex justify-content-center">МАРТ 2019</div></div>
+
+  
+
+   <div class="slider-action d-flex">
+     
+      <div class="slide-list">
+         <div class="slide-wrap">
+             
+            <div class="action-slider-slide-item  d-flex flex-column align-items-center justify-content-center">
+               <div class="slide-img"><img  src="images/slide2.png" alt="" /></div>
+              <div class="slide-item__value size29px brownDark">Утром - скидки на корзинки</div>
+               
+              
+              
+            </div>
+            <div class="action-slider-slide-item  d-flex flex-column align-items-center justify-content-center">
+               <div class="slide-img"><img  src="images/slide3.png" alt="" /></div>
+               <div class="slide-item__value size29px brownDark">Три батона по цене двух</div>
+             
+              
+              
+            </div>
+            <div class="action-slider-slide-item  d-flex flex-column align-items-center justify-content-center">
+               <div class="slide-img"><img  src="images/slide1.png" alt="" /></div>
+               <div class="slide-item__value size29px brownDark">Пробуем трансильванский хлеб</div>
+             
+              
+              
+            </div>
+            <div class="action-slider-slide-item d-flex flex-column align-items-center justify-content-center">
+               <div class="slide-img"><img  src="images/slide4.png" alt="" /></div>
+               <div class="slide-item__value size29px brownDark">Мастер-класс по бутербродам</div>
+              
+              
+              
+               </div>
+            
+             
+            
       </div>
-   
+
+</div></div>  <div class="action-arrow-navy d-flex justify-content-between">
+   <div class="navy prev-slide d-flex"><img src="../images/left-arrow.png"></div>
+   <div class="navy next-slide  d-flex"><img src="../images/right-arrow.png"></div></div>
+ 
+ </div>
+
+</div>
+
+<div class="main-about wrap-main-orderEnd-content-amount d-flex justify-content-between align-items-center">
+    <div class="main-about-row col-xl-3 d-flex flex-column align-items-end">
+        <div class="main-about-row__img"><img src="../images/bulka1.gif"></div>
+        <div class="main-about-row__img"><img src="../images/bulka2.gif"></div>
+        <div class="main-about-row__img"><img src="../images/bulka3.gif"></div>
+    </div>
+    <div class="wrap-main-about-content col-xl-6 d-flex flex-column align-items-center justify-content-center">
+    <div class="main-about-content grandpadding d-flex flex-column align-items-center justify-content-center">
+         <div class="main-about-content-text col-xl-10">
+        <div class="main-about-content__title title_padding fontTahoma size35px white text-center">О НАС</div>
+        <div class="d-flex flex-column align-items-center justify-content-center"><div class="main-about-content__text  col-xl-9 cart-order__name_padding text-center fontSegoePrint align-items-center size29px white">Добро пожаловать в вашу
+новую пекарню!!!У нас работают только лучшие специалисты и тэ дэ и</div></div>
+    
+        <div class="d-flex cart-order_padding justify-content-center"><div class="main-about-content__btn  main-btn_hover d-flex justify-content-center main-grandmenu-block__btn icons__item_padding col-xl-4">
+            <a href="" class="main_btn size29px white">перейти</a>
+        </div></div></div></div></div>
+    <div class="main-about-row col-xl-3 d-flex flex-column align-items-start">
+        <div class="main-about-row__img"><img src="../images/bulka4.gif"></div>
+        <div class="main-about-row__img"><img src="../images/bulka5.gif"></div>
+        <div class="main-about-row__img"><img src="../images/bulka6.gif"></div>
+    </div>
+</div>
+
+
+
+
+ </div>
+ 
+
+
 <?php 
+//require_once('cart-empty.php');
  require_once('footer.php'); ?>
  
     <script src="js/app.min.js"></script>
-     <script src="js/slider.js"></script>
+     
+     <script src="js/action_slider.js"></script>
  </body>
  </html>
