42abede723f695c937631027cba3bfb61e848c34
unknown <marselina88@list.ru>
2022-06-02 19:26:36 +0300

    about
    моб версию надо доделать

---
+++ app/catalog.php
@@ -0,0 +1,97 @@
+<div class="d-flex flex-xl-column flex-lg-column flex-md-row flex-sm-row flex-row">
+<div class="wrap-view-product d-flex flex-column col-xl-11 col-lg-11 col-md-6 col-sm-6 col-6">
+            <div class="d-flex view-product flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column justify-content-center justify-content-sm-start">
+                <div class="view-product-left col-xl-6 col-lg-6 flex-column">
+                <div class="d-flex view-product-left__img justify-content-center">
+                    <img src="/images/page2.png"  alt="" /></div>
+                <div class="wrap-view-product-left__info d-flex justify-content-center"><div class="d-flex view-product-left__info title_padding align-items-start flex-column">
+                 
+                  <div class="brownDark justify-content-center font-weight-bold size24px">200<span class="rubl">₽</span></div>
+                  <div class="view-product-right__status padding_status justify-content-center orange size24px ">Бестселлер</div>
+                  <div class="view-product-right__name_sm bottom_padding"><a href="/product/" class="burgundy  fontSegoePrint font-weight-bold size35px padding-15">Эклер</a>
+                       
+                        <div class="view-product-right__status  brownGrey size16px">нет в наличии</div></div>
+              </div></div>
+                   <div class="d-flex view-product-left__button  justify-content-center">
+                  <button class="d-flex content-order-end__click__btn btn btn-success justify-content-center size29px size20px" type="button">В корзину</button></div>
+                </div>
+
+
+                <div class="view-product-right col-xl-6 col-lg-6 flex-column">
+                    <div class="view-product-right__name bottom_padding"><a href="/product/" class="burgundy  fontSegoePrint font-weight-bold size35px padding-15">Эклер</a>
+                       
+                        <div class="view-product-right__status  brownGrey size22px">нет в наличии</div></div>
+                <div class="view-product-right-info flex-column">
+                <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">+ Описание</div>
+                <div class="bottom_padding col-xl-7 d-flex flex-column brownDark fontSegoePrint size20px line-height1 "><span class="">Свежайшие Пирожные с фруктами и молочным шоколадом</span><span class="">Вес 200 г</span></div>
+               </div>
+
+                <div class="view-product-right-info flex-column"> 
+                <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">+ основные ингредиенты</div><div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span class=""> - Соль</span></div>
+                <div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span> - Мука</span></div>
+                <div class="view-product-right-info__item brownDark bottom_padding fontSegoePrint size20px"><span> - Вода</span></div>
+            </div>
+                <div class="view-product-right-info__slogan view-product-right-info_padding brownLight size35px fontSegoePrint padding-15">Возьми с собой!</div>
+
+ 
+     <div class="view-product-right-info__ing">
+               <div class="wrap-product-right-info__ing1 d-flex"> <div class="view-product-right-info__ing1 view-product-right-info__ing_padding d-flex justify-content-center align-items-center  white size24px fontTahoma">Крем1</div></div>
+                <div class="wrap-product-right-info__ing d-flex justify-content-start"><div class="view-product-right-info__ing2 view-product-right-info__ing_padding d-flex  white justify-content-center align-items-center size24px fontTahoma">Шоколад2
+                </div></div>
+                <div class="wrap-product-right-info__ing3 d-flex">
+                <div class="view-product-right-info__ing3 view-product-right-info__ing_padding d-flex justify-content-center align-items-center white size24px fontTahoma">Фрукты3</div></div>
+            </div></div>
+        </div>
+        <div class="separator col-xl-12 col-lg-12 justify-content-center"><img src="../images/razdel1.png"></div>
+        </div>
+
+
+
+      
+                
+<div class="wrap-view-product d-flex flex-column col-xl-12 col-lg-12  col-md-6 col-sm-6 col-6 ">
+            <div class="d-flex view-product flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column justify-content-center justify-content-sm-start">
+                <div class="view-product-left col-xl-4 flex-column">
+                <div class="d-flex view-product-left__img justify-content-center">
+                    <img src="/images/page3.png"  alt="" /></div>
+                <div class="wrap-view-product-left__info d-flex justify-content-center"><div class="d-flex view-product-left__info title_padding align-items-start flex-column">
+                 
+                  <div class="brownDark justify-content-center font-weight-bold size24px">200<span class="rubl">₽</span></div>
+                  <div class="view-product-right__status bottom_padding justify-content-center orange size24px ">Бестселлер</div>
+                  <div class="view-product-right__name_sm bottom_padding"><a href="/product/" class="burgundy  fontSegoePrint font-weight-bold size35px padding-15">Эклер</a>
+                       
+                        <div class="view-product-right__status  brownGrey size16px">нет в наличии</div></div>
+              </div></div>
+                   <div class="d-flex view-product-left__button  justify-content-center">
+                  <button class="d-flex content-order-end__click__btn btn btn-success justify-content-center size29px col-xl-5 col-lg-5 col-md-4 col-sm-6 col-8" type="button">В корзину</button></div>
+                </div>
+
+
+                <div class="view-product-right col-xl-6 flex-column">
+                    <div class="view-product-right__name bottom_padding"><a href="/product/" class="burgundy  fontSegoePrint font-weight-bold size35px padding-15">Эклер</a>
+                       
+                        <div class="view-product-right__status  brownGrey size22px">нет в наличии</div></div>
+                <div class="view-product-right-info flex-column">
+                <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">+ Описание</div>
+                <div class="bottom_padding col-xl-7 d-flex flex-column brownDark fontSegoePrint size20px line-height1 "><span class="">Свежайшие Пирожные с фруктами и молочным шоколадом</span><span class="">Вес 200 г</span></div>
+               </div>
+
+                <div class="view-product-right-info flex-column"> 
+                <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">+ основные ингредиенты</div><div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span class=""> - Соль</span></div>
+                <div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span> - Мука</span></div>
+                <div class="view-product-right-info__item brownDark bottom_padding fontSegoePrint size20px"><span> - Вода</span></div>
+            </div>
+                <div class="view-product-right-info__slogan view-product-right-info_padding brownLight size35px fontSegoePrint padding-15">Возьми с собой!</div>
+
+ 
+     <div class="view-product-right-info__ing">
+               <div class="wrap-product-right-info__ing1 d-flex"> <div class="view-product-right-info__ing1 view-product-right-info__ing_padding d-flex justify-content-center align-items-center  white size24px fontTahoma">Крем1</div></div>
+                <div class="wrap-product-right-info__ing d-flex justify-content-start"><div class="view-product-right-info__ing2 view-product-right-info__ing_padding d-flex  white justify-content-center align-items-center size24px fontTahoma">Шоколад2
+                </div></div>
+                <div class="wrap-product-right-info__ing3 d-flex">
+                <div class="view-product-right-info__ing3 view-product-right-info__ing_padding d-flex justify-content-center align-items-center white size24px fontTahoma">Фрукты3</div></div>
+            </div></div>
+        </div>
+        <div class="separator col-xl-12 col-lg-12 justify-content-center"><img src="../images/razdel1.png"></div>
+        </div>              
+            </div>
--- app/index.php
+++ app/index.php
@@ -37,114 +37,44 @@
 
 ?>
 
-<?php //require_once('start.php');    ?>   
+<?php //require_once('start.php'); 
+//require_once('catalog.php');   ?>   
 <div class="wrap-content">
     <div class="main">
      <div class="header-logo col-xl-10 col-lg-10 offset-xl-1 offset-lg-1 col-md-10 offset-md-1 col-sm-10 offset-sm-1 d-flex justify-content-between align-items-center"><div class="header-logo-menu__logo"><img src="../images/logo.png" alt=""></div><div class="header-logo-menu__menu"><img src="../images/menu.png" alt=""></div></div>
 
 
-<?php require_once('menu_category.php');?>
-<div class="d-flex flex-xl-column flex-lg-column flex-md-row flex-sm-row flex-row">
-<div class="wrap-view-product d-flex flex-column col-xl-11 col-lg-11 col-md-6 col-sm-6 col-6">
-            <div class="d-flex view-product flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column justify-content-center justify-content-sm-start">
-                <div class="view-product-left col-xl-6 col-lg-6 flex-column">
-                <div class="d-flex view-product-left__img justify-content-center">
-                    <img src="/images/page2.png"  alt="" /></div>
-                <div class="wrap-view-product-left__info d-flex justify-content-center"><div class="d-flex view-product-left__info title_padding align-items-start flex-column">
-                 
-                  <div class="brownDark justify-content-center font-weight-bold size24px">200<span class="rubl">₽</span></div>
-                  <div class="view-product-right__status bottom_padding justify-content-center orange size24px ">Бестселлер</div>
-                  <div class="view-product-right__name_sm bottom_padding"><a href="/product/" class="burgundy  fontSegoePrint font-weight-bold size35px padding-15">Эклер</a>
-                       
-                        <div class="view-product-right__status  brownGrey size16px">нет в наличии</div></div>
-              </div></div>
-                   <div class="d-flex view-product-left__button  justify-content-center">
-                  <button class="d-flex content-order-end__click__btn btn btn-success justify-content-center size29px size20px" type="button">В корзину</button></div>
-                </div>
+<?php //require_once('menu_category.php');?>
 
-
-                <div class="view-product-right col-xl-6 col-lg-6 flex-column">
-                    <div class="view-product-right__name bottom_padding"><a href="/product/" class="burgundy  fontSegoePrint font-weight-bold size35px padding-15">Эклер</a>
-                       
-                        <div class="view-product-right__status  brownGrey size22px">нет в наличии</div></div>
-                <div class="view-product-right-info flex-column">
-                <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">+ Описание</div>
-                <div class="bottom_padding col-xl-7 d-flex flex-column brownDark fontSegoePrint size20px line-height1 "><span class="">Свежайшие Пирожные с фруктами и молочным шоколадом</span><span class="">Вес 200 г</span></div>
-               </div>
-
-                <div class="view-product-right-info flex-column"> 
-                <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">+ основные ингредиенты</div><div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span class=""> - Соль</span></div>
-                <div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span> - Мука</span></div>
-                <div class="view-product-right-info__item brownDark bottom_padding fontSegoePrint size20px"><span> - Вода</span></div>
-            </div>
-                <div class="view-product-right-info__slogan view-product-right-info_padding brownLight size35px fontSegoePrint padding-15">Возьми с собой!</div>
-
- 
-     <div class="view-product-right-info__ing">
-               <div class="wrap-product-right-info__ing1 d-flex"> <div class="view-product-right-info__ing1 view-product-right-info__ing_padding d-flex justify-content-center align-items-center  white size24px fontTahoma">Крем1</div></div>
-                <div class="wrap-product-right-info__ing d-flex justify-content-start"><div class="view-product-right-info__ing2 view-product-right-info__ing_padding d-flex  white justify-content-center align-items-center size24px fontTahoma">Шоколад2
-                </div></div>
-                <div class="wrap-product-right-info__ing3 d-flex">
-                <div class="view-product-right-info__ing3 view-product-right-info__ing_padding d-flex justify-content-center align-items-center white size24px fontTahoma">Фрукты3</div></div>
-            </div></div>
-        </div>
-        <div class="separator col-xl-12 col-lg-12 justify-content-center"><img src="../images/razdel1.png"></div>
-        </div>
-
-
-
-      
-                
-<div class="wrap-view-product d-flex flex-column col-xl-12 col-lg-12  col-md-6 col-sm-6 col-6 ">
-            <div class="d-flex view-product flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column justify-content-center justify-content-sm-start">
-                <div class="view-product-left col-xl-4 flex-column">
-                <div class="d-flex view-product-left__img justify-content-center">
-                    <img src="/images/page3.png"  alt="" /></div>
-                <div class="wrap-view-product-left__info d-flex justify-content-center"><div class="d-flex view-product-left__info title_padding align-items-start flex-column">
-                 
-                  <div class="brownDark justify-content-center font-weight-bold size24px">200<span class="rubl">₽</span></div>
-                  <div class="view-product-right__status bottom_padding justify-content-center orange size24px ">Бестселлер</div>
-                  <div class="view-product-right__name_sm bottom_padding"><a href="/product/" class="burgundy  fontSegoePrint font-weight-bold size35px padding-15">Эклер</a>
-                       
-                        <div class="view-product-right__status  brownGrey size16px">нет в наличии</div></div>
-              </div></div>
-                   <div class="d-flex view-product-left__button  justify-content-center">
-                  <button class="d-flex content-order-end__click__btn btn btn-success justify-content-center size29px col-xl-5 col-lg-5 col-md-4 col-sm-6 col-8" type="button">В корзину</button></div>
-                </div>
-
-
-                <div class="view-product-right col-xl-6 flex-column">
-                    <div class="view-product-right__name bottom_padding"><a href="/product/" class="burgundy  fontSegoePrint font-weight-bold size35px padding-15">Эклер</a>
-                       
-                        <div class="view-product-right__status  brownGrey size22px">нет в наличии</div></div>
-                <div class="view-product-right-info flex-column">
-                <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">+ Описание</div>
-                <div class="bottom_padding col-xl-7 d-flex flex-column brownDark fontSegoePrint size20px line-height1 "><span class="">Свежайшие Пирожные с фруктами и молочным шоколадом</span><span class="">Вес 200 г</span></div>
-               </div>
-
-                <div class="view-product-right-info flex-column"> 
-                <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">+ основные ингредиенты</div><div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span class=""> - Соль</span></div>
-                <div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span> - Мука</span></div>
-                <div class="view-product-right-info__item brownDark bottom_padding fontSegoePrint size20px"><span> - Вода</span></div>
-            </div>
-                <div class="view-product-right-info__slogan view-product-right-info_padding brownLight size35px fontSegoePrint padding-15">Возьми с собой!</div>
-
- 
-     <div class="view-product-right-info__ing">
-               <div class="wrap-product-right-info__ing1 d-flex"> <div class="view-product-right-info__ing1 view-product-right-info__ing_padding d-flex justify-content-center align-items-center  white size24px fontTahoma">Крем1</div></div>
-                <div class="wrap-product-right-info__ing d-flex justify-content-start"><div class="view-product-right-info__ing2 view-product-right-info__ing_padding d-flex  white justify-content-center align-items-center size24px fontTahoma">Шоколад2
-                </div></div>
-                <div class="wrap-product-right-info__ing3 d-flex">
-                <div class="view-product-right-info__ing3 view-product-right-info__ing_padding d-flex justify-content-center align-items-center white size24px fontTahoma">Фрукты3</div></div>
-            </div></div>
-        </div>
-        <div class="separator col-xl-12 col-lg-12 justify-content-center"><img src="../images/razdel1.png"></div>
-        </div>              
-            </div>
 
 
 <?php //require_once('admin_index.php');?>
+<div class="content-about col-xl-12">
+    <div class="size24px brownDark fontTahoma text-center menu-category_padding">ЖДЕМ ВАС ПО АДРЕСУ: УЛИЦА КИРОВА, СТРОЕНИЕ 5</div>
+    <div class="d-flex align-items-center col-xl-10 offset-xl-1 col-lg-12 
+    col-md-12 col-sm-12 col-12  flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">
+    <div class="content-about__img1 content-about__img col-xl-6 col-lg-6 col-md-6 col-sm-10 col-12"><img src="../images/polky.png">
+   </div>
+    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 content-about__text fontSegoePrint size24px">Наш хлеб выпекается по старинным</br>
+проверенным технологиям</div>
+    </div>
 
+<div class="d-flex align-items-center col-xl-10 offset-xl-1 col-lg-12 flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">
+     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 content-about__img2 content-about__img"><img src="../images/povar.png"></div>
+ <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 fontSegoePrint brownDark size24px"><div class="">У нас работают только</br>
+лучшие специалисты</div></div></div>
+
+    <div class="d-flex align-items-center col-xl-10 offset-xl-1 col-lg-12 flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">  
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 content-about__img3 content-about__img"><img src="../images/pechka.png"></div>
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 fontSegoePrint brownDark size24px">Мы используем новейшее оборудование,</br>
+имитирующее русcкую печь</div></div>
+
+ <div class="d-flex align-items-center col-xl-10 offset-xl-1 col-lg-12 flex-xl-row flex-lg-row flex-md-row flex-sm-column flex-column">     
+<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 content-about__img4 content-about__img"><img src="../images/cafe.png"></div>
+ <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 fontSegoePrint brownDark size24px">Мы открываемся в 5 утра,</br>
+вы всегда успеете позавтракать</div></div>
+        
+</div>
 
 
  </div>
